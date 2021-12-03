78cf43d1b0f1f250f8858d9e4be7ecae87da1217
tombricks <tombricks@outlook.com>
2021-12-03 22:31:37 +0000

    Ideology Generator

--- IdeologyGen.py
+++ IdeologyGen.py
@@ -1,7 +1,6 @@
-titles = [
-	["Chairman", "Chairman"],
-	["Acting_General_Secretary", "Acting General Secretary"],
-	["President", "President"]
+ideologies = [
+	["Iosif_Stalin_Thought", "Iosif Stalin Thought", "Iosif Stalin Thought"],
+	["Marxism_Leninism_Maoism", "Marxism-Leninism-Maoism", "Marxist-Leninist-Maoist"]
 ]
 
 # TRAITS
@@ -9,8 +8,8 @@ File_Traits = open("common/country_leader/01_traits.txt", "r", encoding="utf8")
 File_Traits_Text = File_Traits.read()
 File_Traits.close()
 
-for title in titles:
-	File_Traits_Text = File_Traits_Text.replace("	# <END OF TITLES>", F"	TITLE_{title[0]} = {{ random = no }} # {title[1]}\n	# <END OF TITLES>")
+for ideology in ideologies:
+	File_Traits_Text = File_Traits_Text.replace("	# <END OF IDEOLOGIES>", F"	IDEOLOGY_{ideology[0]} = {{ random = no }} # {ideology[1]}\n	# <END OF IDEOLOGIES>")
 
 print("--- common/country_leader/01_traits.txt ---")
 print(File_Traits_Text)
@@ -24,8 +23,8 @@ File_Localisation = open("localisation/english/parties_l_english.yml", "r", enco
 File_Localisation_Text = File_Localisation.read()
 File_Localisation.close()
 
-for title in titles:
-	File_Localisation_Text = File_Localisation_Text.replace(" # <END OF TITLES>", F" TITLE_{title[0]}: \"{title[1]}\"\n # <END OF TITLES>")
+for ideology in ideologies:
+	File_Localisation_Text = File_Localisation_Text.replace(" # <END OF IDEOLOGIES>", F" IDEOLOGY_{ideology[0]}: \"{ideology[1]}\"\n IDEOLOGY_{ideology[0]}_ADJ: \"{ideology[2]}\"\n # <END OF IDEOLOGIES>")
 
 print("--- localisation/english/parties_l_english.yml ---")
 print(File_Localisation_Text)
@@ -39,8 +38,9 @@ File_Scripted_Localisation = open("common/scripted_localisation/politics_scripte
 File_Scripted_Localisation_Text = File_Scripted_Localisation.read()
 File_Scripted_Localisation.close()
 
-for title in titles:
-	File_Scripted_Localisation_Text = File_Scripted_Localisation_Text.replace("	# <END OF TITLES>", F"	text = {{ trigger = {{ has_country_leader_with_trait = TITLE_{title[0]} }} localization_key = TITLE_{title[0]} }} # {title[1]}\n	# <END OF TITLES>")
+for ideology in ideologies:
+	File_Scripted_Localisation_Text = File_Scripted_Localisation_Text.replace("	# <END OF IDEOLOGIES>", F"	text = {{ trigger = {{ has_country_leader_with_trait = IDEOLOGY_{ideology[0]} }} localization_key = IDEOLOGY_{ideology[0]} }} # {ideology[1]}\n	# <END OF IDEOLOGIES>")
+	File_Scripted_Localisation_Text = File_Scripted_Localisation_Text.replace("	# <END OF IDEOLOGIES ADJ>", F"	text = {{ trigger = {{ has_country_leader_with_trait = IDEOLOGY_{ideology[0]} }} localization_key = IDEOLOGY_{ideology[0]}_ADJ }} # {ideology[2]}\n	# <END OF IDEOLOGIES ADJ>")
 
 print("--- common/scripted_localisation/politics_scripted_localisation.txt ---")
 print(File_Scripted_Localisation_Text)
