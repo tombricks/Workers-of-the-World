917f97930366f7ad84098bd056006bf75cd4af8f
tombricks <tombricks@outlook.com>
2022-01-23 15:42:47 +0000

    eek

--- CharacterGen.py
+++ CharacterGen.py
@@ -1,12 +1,152 @@
 import os
 
 characters = [
-	{
-		"tag": "SWE",
-		"id": "Frank_Baude",
-		"name": "Frank Baude",
+	{ # https://en.wikipedia.org/wiki/Communist_Party_of_Norway
+		"tag": "NOR",
+		"id": "Martin_Gunnar_Knutsen",
+		"name": "Martin Gunnar Knutsen",
 		"leaders": {
 			"Marxism_Leninism": [ "TITLE_Chairman", "IDEOLOGY_Marxism_Leninism" ]
+		},
+		"advisors": {
+		}
+	},
+	{ # https://en.wikipedia.org/wiki/Communist_Party_of_Malta
+		"tag": "MLT",
+		"id": "Anthony_Vassallo",
+		"name": "Anthony Vassallo",
+		"leaders": {
+			"Marxism_Leninism": [ "TITLE_General_Secretary", "IDEOLOGY_Marxism_Leninism" ]
+		},
+		"advisors": {
+		}
+	},
+	{ # https://en.wikipedia.org/wiki/Tudeh_Party_of_Iran
+		"tag": "IRN",
+		"id": "Iraj_Eskandari",
+		"name": "Iraj Eskandari",
+		"leaders": {
+			"Marxism_Leninism": [ "TITLE_General_Secretary", "IDEOLOGY_Marxism_Leninism" ]
+		},
+		"advisors": {
+		}
+	},
+	{ # https://en.wikipedia.org/wiki/Palestine_Liberation_Organization
+		"tag": "PAL",
+		"id": "Yasser_Arafat",
+		"name": "Yasser Arafat",
+		"leaders": {
+			"Socialism": [ "TITLE_Chairman", "IDEOLOGY_Arab_Socialism" ]
+		},
+		"advisors": {
+		}
+	},
+	{ # https://en.wikipedia.org/wiki/Al-Jama%27a_al-Islamiyya
+		"tag": "ISI",
+		"id": "Omar_Abdel_Rahman",
+		"name": "Omar Abdel-Rahman",
+		"leaders": {
+			"Reactionism": [ "TITLE_Sheikh", "IDEOLOGY_Islamism" ]
+		},
+		"advisors": {
+		}
+	},
+	{ # https://en.wikipedia.org/wiki/Communist_Party_of_Turkey_(historical)
+		"tag": "TUR",
+		"id": "Ismail_Bilen",
+		"name": "İsmail Bilen",
+		"leaders": {
+			"Marxism_Leninism": [ "TITLE_General_Secretary", "IDEOLOGY_Marxism_Leninism" ]
+		},
+		"advisors": {
+		}
+	},
+	{ # https://en.wikipedia.org/wiki/People%27s_Democratic_Party_of_Afghanistan
+		"tag": "AFG",
+		"id": "Hafizullah_Amin",
+		"name": "Hafizullah Amin",
+		"leaders": {
+			"Marxism_Leninism": [ "TITLE_General_Secretary", "IDEOLOGY_Marxism_Leninism" ]
+		},
+		"advisors": {
+		}
+	},
+	{ # Unified Communist Party https://en.wikipedia.org/wiki/Communist_Party_of_India_(Marxist)
+		"tag": "IND",
+		"id": "E_M_S_Namboodiripad",
+		"name": "E. M. S. Namboodiripad",
+		"leaders": {
+			"Marxism_Leninism": [ "TITLE_General_Secretary", "IDEOLOGY_Marxism_Leninism" ]
+		},
+		"advisors": {
+		}
+	},
+	{ # https://en.wikipedia.org/wiki/Awami_Tahreek#History
+		"tag": "PAK",
+		"id": "Rasul_Bux_Palejo",
+		"name": "Rasul Bux Palejo",
+		"leaders": {
+			"New_Marxism": [ "TITLE_President", "IDEOLOGY_Marxism_Leninism_Maoism" ]
+		},
+		"advisors": {
+		}
+	},
+	{ # https://en.wikipedia.org/wiki/Bangladesh_National_Awami_Party
+		"tag": "BNG",
+		"id": "Muzaffar_Ahmed",
+		"name": "Muzaffar Ahmed",
+		"leaders": {
+			"Marxism_Leninism": [ "TITLE_President", "IDEOLOGY_Marxism_Leninism" ]
+		},
+		"advisors": {
+		}
+	},
+	{ # https://en.wikipedia.org/wiki/Communist_Party_of_Nepal_(Fourth_Convention)
+		"tag": "NEP",
+		"id": "Mohan_Bikram_Singh",
+		"name": "Mohan Bikram Singh",
+		"leaders": {
+			"New_Marxism": [ "TITLE_General_Secretary", "IDEOLOGY_Marxism_Leninism_Maoism" ]
+		},
+		"advisors": {
+		}
+	},
+	{ # a momment
+		"tag": "BHU",
+		"id": "Sherub_Gyeltshen",
+		"name": "Sherub Gyeltshen",
+		"leaders": {
+			"New_Marxism": [ "TITLE_General_Secretary", "IDEOLOGY_Marxism_Leninism_Maoism" ]
+		},
+		"advisors": {
+		}
+	},
+	{ # https://en.wikipedia.org/wiki/Communist_Party_of_Burma
+		"tag": "BRM",
+		"id": "Thakin_Ba_Thein_Tin",
+		"name": "Thakin Ba Thein Tin",
+		"leaders": {
+			"New_Marxism": [ "TITLE_Chairman", "IDEOLOGY_Marxism_Leninism_Maoism" ]
+		},
+		"advisors": {
+		}
+	},
+	{ # https://en.wikipedia.org/wiki/Lao_People%27s_Revolutionary_Party
+		"tag": "LAO",
+		"id": "Kaysone_Phomvihane",
+		"name": "Kaysone Phomvihane",
+		"leaders": {
+			"Marxism_Leninism": [ "TITLE_Chairman", "IDEOLOGY_Marxism_Leninism" ]
+		},
+		"advisors": {
+		}
+	},
+	{ # https://en.wikipedia.org/wiki/Communist_Party_of_Vietnam
+		"tag": "VIE",
+		"id": "Ton_Duc_Thang",
+		"name": "Tôn Đức Thắng",
+		"leaders": {
+			"Marxism_Leninism": [ "TITLE_President", "IDEOLOGY_Marxism_Leninism" ]
 		},
 		"advisors": {
 		}
