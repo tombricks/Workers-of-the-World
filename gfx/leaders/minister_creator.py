23eae59a84dcaa4f11b9acab05b5d5ca5a1e2d13
tombricks <tombricks@outlook.com>
2022-03-05 23:47:33 +0000

    sex offender

--- gfx/leaders/minister_creator.py
+++ gfx/leaders/minister_creator.py
@@ -9,7 +9,7 @@ for folder in os.walk('.'):
         for orig_file_name in folder[2]:
             if not os.path.isfile((folder[0] + "\\" + orig_file_name.replace(".dds", "") + " minister.dds")) and "minister" not in orig_file_name:
                 print(folder[0] + "\\" + orig_file_name)
-                orig_file = Image.open(folder[0] + "\\" + orig_file_name)
+                orig_file = Image.open(folder[0] + "\\" + orig_file_name).crop((0, 0, 155, 209))
                 new_file = template_file
                 paste = orig_file.resize((35, 48))
                 new_file.paste(paste, (15, 9))
